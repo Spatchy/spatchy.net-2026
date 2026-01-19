6270987024bae9105e8bc8a2b1049e0de65027ee
Spatchy <spatchy@spatchy.net>
2026-01-19 23:23:33 +0000

    Add vector favicon

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,7 @@ export default define.page(function App({ Component }) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>spatchy.net</title>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml"></link>
       </head>
       <body>
         <Component />
